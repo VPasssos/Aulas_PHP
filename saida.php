c1c11d64fc87befc80685f1fc0a6c4e312231278
Vinicius dos Passos da Silva <viniciuspassos030@gmail.com>
2025-07-08 22:08:53 -0300

    Códigos de Exemplos

---
+++ saida.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Saida do console</title>
+</head>
+<body>
+    <?php
+    
+    print "teste<br>";
+    print "Olá, mundo<br>";
+    print "Escape 'chars' são os MESMOS como em C<br>";
+    print "Você pode ter quebras de linha em uma string.<br> ";
+    print "Uma string pode usar 'aspas-duplas'. Isso é meito legal! <br> ";
+    print "Aida pode-se usar apas simples dessa forma's cool!'<br>";
+
+
+    echo "<br>teste<br>";
+    echo "Olá, mundo<br>";
+    echo "Isso abrange varias linhas. As novas linhas serão saida tambem<br>";
+    echo "Isso abrage<br>multiplos linhas. A nova linha será <br> a saida tambem <br> ";
+    echo "Caracteres Escaping são feitos \"como esse\"."
+
+    
+    ?>
+</body>
+</html>
