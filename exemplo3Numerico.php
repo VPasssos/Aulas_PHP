288627b4a33e1f9e60bd590d023741152799bb41
Vinicius dos Passos da Silva <viniciuspassos030@gmail.com>
2025-07-09 21:37:13 -0300

    Exemplo Numerioco
    Exemplo disponibilizado na aula de PHP

---
+++ exemplo3Numerico.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $num = 537; //Numero Inteiro
+    echo $num, " = Numero Inteiro <br/><br/>";
+    $num = -13 ; //Numero Negativo
+    echo $num, " = Numero Negativo <br/><br/>";
+    $num = 0123; //Numero Octal (equivalente a 83 em decimal)
+    echo $num, " = Numero Octal <br/><br/>";
+    $num = 0x1A; //Numero hexadecimal (equivalente a 26 decimal)
+    echo $num, " = Numero Inteiro <br/><br/>";
+    $num = -13; //Ponto Flutuante Ciêntifica (equilvalente a 83 em decimal)
+    echo $num, " = Numero Ciêntifica <br/><br/>";
+
+    
+    
+    
+    ?>
+</body>
+</html>
